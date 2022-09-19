cb5cf45a6889751a292939542bdf9afb7d9a7ff2
gmirg <gofthet@gmail.com>
2022-09-19 23:18:35 +0700

    ejercicios lunes 19

---
+++ ej5.5/script.js
@@ -0,0 +1,8 @@
+var n = window.prompt("Inserta un número entero: ");
+alert(n + n + 2);
+n = parseInt(n);
+alert(n + n + 2);
+var n2 = window.prompt("Inserta un número real: ");
+alert(n2 + n2 + 2);
+n2 = parseFloat(n2);
+alert(n2 + n2 + 2);
---
+++ js5.4/script.js
@@ -0,0 +1,10 @@
+let frutas = ["Manzana", "Banana"];
+console.log(frutas);
+function add() {
+    frutas.push('Naranja');
+    alert(frutas)
+} 
+function quitar() {
+    frutas.pop();
+    alert(frutas)
+}
